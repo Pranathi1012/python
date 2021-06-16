9fa350e2a22140f3b72dc87384c030b45af11355
Pranathi1012 <85788425+Pranathi1012@users.noreply.github.com>
2021-06-16 20:27:33 +0530

    Add files via upload

---
+++ spypytrail.py
@@ -0,0 +1,31 @@
+list = ["pranathi","19","sathyabama university"]
+list.append("chennai")
+print(list)
+
+a=list.copy()
+print(a)
+
+list1 = ["pranu","20","female"]
+list1.insert(3,"CSE")
+print(list1)
+
+list1.remove("20")
+print(list1)
+
+print(len(list))
+
+list.pop(1)
+print(list)
+
+list1.reverse()
+print(list1)
+
+list.extend(list1)
+print(list1)
+
+list.sort()
+print(list)
+
+list.clear()
+print(list)
+
