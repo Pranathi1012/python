a24044015d578392538d1e37c536534687b403c2
Pranathi1012 <85788425+Pranathi1012@users.noreply.github.com>
2021-06-16 16:43:50 +0530

    Add files via upload

---
+++ spy trail.py
@@ -0,0 +1,12 @@
+a=5
+print(a)
+
+
+list = ["pranathi","sathyabama","chennai"]
+list.append("india")
+print(list)
+
+
+list = ["pranathi","sathyabama","chennai","india"]
+list.remove("india")
+print(list)
